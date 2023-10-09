a9307da05a5b1bac0d6c89c496ae1c3868c459e0
Antonio Llamas <antoniollamasln@gmail.com>
2023-10-09 09:40:13 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// 1 Suma todos los elementos de un array.
+
+// 2 Imprime todos los elementos de un array.
+
+// 3 Duplica cada elemento de un array y crea un nuevo array con los duplicados.
+
+// 4 Encuentra el elemento más grande en un array.
+
+// 5 Filtra elementos en un array y crea un nuevo array con los elementos que cumplan cierta condición.
+
